fb6708a7a1167ae0fee990f3717b9c892020f5b4
Peemai124 <nongmai_kitchakoot@hotmail.com>
2018-08-03 19:06:41 +0700

    add chart success

--- src/pages/follow-tax-mth/follow-tax-mth.ts
+++ src/pages/follow-tax-mth/follow-tax-mth.ts
@@ -59,47 +59,51 @@ export class FollowTaxMthPage {
       //  return this.respondData[i];
     } 
     
-    for (var i = 0; i < other.length; i++) {
-      //for(let data of this.respondData) {
-       // amts.push(other[i].AMT);
-    
-        //console.log(this.respondData[i]);
-      //  return this.respondData[i];
-    }
-
-   // console.log(JSON.stringify(amts));
-
-    this.lineChart = new Chart(this.lineCanvas.nativeElement, {
-      type: 'line',
-      data: {
-          labels: ["ภาค1", "ภาค2", "ภาค3", "ภาค4", "ภาค5", "ภาค6", "ภาค7", "ภาค8", "ภาค9", "ภาค10", "ไม่ระบุภาค"],
-          datasets: [
-              {
-                  label: "Sell per week",
-                  fill: false,
-                  lineTension: 0.1,
-                  backgroundColor: "rgba(75,192,192,0.4)",
-                  borderColor: "rgba(75,192,192,1)",
-                  borderCapStyle: 'butt',
-                  borderDash: [],
-                  borderDashOffset: 0.0,
-                  borderJoinStyle: 'miter',
-                  pointBorderColor: "rgba(75,192,192,1)",
-                  pointBackgroundColor: "#fff",
-                  pointBorderWidth: 1,
-                  pointHoverRadius: 5,
-                  pointHoverBackgroundColor: "rgba(75,192,192,1)",
-                  pointHoverBorderColor: "rgba(220,220,220,1)",
-                  pointHoverBorderWidth: 2,
-                  pointRadius: 1,
-                  pointHitRadius: 10,
-                  data: [amts.push(other[i].AMT)],// ],// [65, 59, 80, 81, 56, 55, 40, 10, 5, 50, 10, 15],
-                  spanGaps: false,
-              }
-          ]
+      for (var i = 0; i < other.length; i++) {
+        amts.push(other[i].AMT);
       }
 
-  });
-
+      return JSON.stringify(amts);
+   // console.log(JSON.stringify(amts));
     }
+
+testgraph(){
+  let other =  JSON.parse(this.showStreets());
+  console.log(other);
+
+  this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+    type: 'line',
+    data: {
+        labels: ["ภาค1", "ภาค2", "ภาค3", "ภาค4", "ภาค5", "ภาค6", "ภาค7", "ภาค8", "ภาค9", "ภาค10", "ไม่ระบุภาค"],
+        datasets: [
+            {
+                label: "Sell per week",
+                fill: false,
+                lineTension: 0.1,
+                backgroundColor: "rgba(75,192,192,0.4)",
+                borderColor: "rgba(75,192,192,1)",
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: "rgba(75,192,192,1)",
+                pointBackgroundColor: "#fff",
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                pointHoverBorderColor: "rgba(220,220,220,1)",
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: other,// ],// [65, 59, 80, 81, 56, 55, 40, 10, 5, 50, 10, 15],
+                spanGaps: false,
+            }
+        ]
+    }
+
+});
+
+}
+
+
 }
