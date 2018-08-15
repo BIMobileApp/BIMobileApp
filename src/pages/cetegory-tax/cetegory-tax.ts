da323f83805ef8fd28ae40cc7d5871d50f2ef003
Juthamart <Juthamart.k@v-smart.com>
2018-08-15 18:32:06 +0700

    edit overview page

--- src/pages/cetegory-tax/cetegory-tax.ts
+++ src/pages/cetegory-tax/cetegory-tax.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams,App } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, App } from 'ionic-angular';
+import { RestProvider } from '../../providers/rest/rest';
 import { TaxbudgetyearPage } from '../taxbudgetyear/taxbudgetyear';
 import { TaxAllCountryPage } from '../tax-all-country/tax-all-country';
 import { TaxMonthlyFreezonePage } from '../tax-monthly-freezone/tax-monthly-freezone';
+import { TaxCoutrySection1Page } from '../tax-coutry-section1/tax-coutry-section1';
+import { TaxCoutrySection2Page } from '../tax-coutry-section2/tax-coutry-section2';
+import { TaxCoutrySection3Page } from '../tax-coutry-section3/tax-coutry-section3';
+import { TaxCoutrySection4Page } from '../tax-coutry-section4/tax-coutry-section4';
+import { TaxCoutrySection5Page } from '../tax-coutry-section5/tax-coutry-section5';
+import { TaxCoutrySection6Page } from '../tax-coutry-section6/tax-coutry-section6';
+import { TaxCoutrySection7Page } from '../tax-coutry-section7/tax-coutry-section7';
+import { TaxCoutrySection8Page } from '../tax-coutry-section8/tax-coutry-section8';
+import { TaxCoutrySection9Page } from '../tax-coutry-section9/tax-coutry-section9';
+import { TaxCoutrySection10Page } from '../tax-coutry-section10/tax-coutry-section10';
 
 @IonicPage()
 @Component({
@@ -10,6 +21,124 @@ import { TaxMonthlyFreezonePage } from '../tax-monthly-freezone/tax-monthly-free
   templateUrl: 'cetegory-tax.html',
 })
 export class CetegoryTaxPage {
+  //map parm
+  offcode: any;
+
+  //guage parm
+  TaxGauge: any;
+  TaxlyGauge: any;
+  TaxEstGauge: any;
+
+  //Table parm
+  responseData: any;
+  Data = [];
+  TAX = [];
+  TAX_LY = [];
+  EST = [];
+
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public app: App,
+    public webapi:RestProvider) {
+  }
+
+  ionViewDidLoad() {
+    this.UserAthu();
+    this.TableGetData();
+
+  }
+
+  UserAthu() {
+    this.offcode = localStorage.offcode;
+    this.offcode = this.offcode.substring(0, 2);
+    console.log(this.offcode);
+  }
+
+  TableGetData(){
+    this.webapi.getData('TaxBudgetYear').then((data)=>{
+      this.responseData = data;
+
+      //console.log(this.responseData);
+      this.getTAX();
+      this. getTAX_LY();
+      this.getEST();
+    });
+  }
+
+  getTAX() {
+    this.TAX = [];
+    let val;
+    for (var i = 0; i < this.responseData.length; i++) {
+      val = this.responseData[i].TAX/1000000;
+      val = val.toFixed(2);
+      val = val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      this.responseData[i].TAX = val;
+    }
+  }
+
+
+
+  getTAX_LY() {
+    this.TAX_LY = [];
+    let val;
+    for (var i = 0; i < this.responseData.length; i++) {
+      val = this.responseData[i].TAX_LY/1000000;
+      val = val.toFixed(2);
+      val = val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      this.responseData[i].TAX_LY = val;
+    }
+  }
+
+  getEST() {
+    this.EST = [];
+    let val;
+    for (var i = 0; i < this.responseData.length; i++) {
+      val = this.responseData[i].ESTIMATE/1000000
+      val = val.toFixed(2);
+      val = val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      this.responseData[i].ESTIMATE = val;
+    }
+  }
+
+
+  section1() {
+    this.app.getRootNav().push(TaxCoutrySection1Page);
+  }
+  section2() {
+    this.app.getRootNav().push(TaxCoutrySection2Page);
+  }
+  section3() {
+    this.app.getRootNav().push(TaxCoutrySection3Page);
+  }
+  section4() {
+    this.app.getRootNav().push(TaxCoutrySection4Page);
+  }
+  section5() {
+    this.app.getRootNav().push(TaxCoutrySection5Page);
+  }
+  section6() {
+    this.app.getRootNav().push(TaxCoutrySection6Page);
+  }
+  section7() {
+    this.app.getRootNav().push(TaxCoutrySection7Page);
+  }
+  section8() {
+    this.app.getRootNav().push(TaxCoutrySection8Page);
+  }
+  section9() {
+    this.app.getRootNav().push(TaxCoutrySection9Page);
+  }
+  section10() {
+    this.app.getRootNav().push(TaxCoutrySection10Page);
+  }
+
+
+
+}
+
+
+
+/*export class CetegoryTaxPage {
 
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
@@ -33,3 +162,4 @@ export class CetegoryTaxPage {
   }
 
 }
+*/
